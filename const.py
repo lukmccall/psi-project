5888858338582d49213abadfc4451d49bc87ca66
Łukasz Kosmaty <lukasz.kosmaty@swmansion.com>
2020-06-05 14:39:11 +0200

    Add preprocessing

---
+++ const.py
@@ -0,0 +1,4 @@
+DATASET_PATH = "training.1600000.processed.noemoticon 2.csv"
+W2V_MODEL_PATH = "W2V.model"
+PREPROCESSED_TEST_PATH = "sentimental140_test.pkl"
+PREPROCESSED_TRAIN_PATH = "sentimental140_train.pkl"
