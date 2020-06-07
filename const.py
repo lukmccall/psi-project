a56764377d3699701c6c60c3327c05c3822e1314
lukmccall <kosmatylukasz@gmail.com>
2020-06-07 15:55:49 +0200

    Learn ANN

--- const.py
+++ const.py
@@ -1,4 +1,4 @@
-DATASET_PATH = "training.1600000.processed.noemoticon 2.csv"
+DATASET_PATH = "training.1600000.processed.noemoticon.csv"
 W2V_MODEL_PATH = "W2V.model"
 PREPROCESSED_TEST_PATH = "sentimental140_test.pkl"
 PREPROCESSED_TRAIN_PATH = "sentimental140_train.pkl"
